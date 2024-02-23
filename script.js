ecc7ad5d92a2dab96d4f2f673929b43dd0e7737a
Brandon <BrandonKuffWork@gmail.com>
2024-02-23 16:17:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function togglemenu() {
     menu.classList.toggle('open'); 
     icon.classList.toggle('open'); 
 }
+
+// AWS Lambda Code
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://dqvypqwpctv3stkvil6zhs67ne0myrja.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+
+updateCounter();
