51f4e3126a2190a1f1b9fe41ab6abad5ab85a4a6
Brandon <118094542+BrandonVex@users.noreply.github.com>
2024-07-16 15:45:53 -0700

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -11,5 +11,6 @@ async function updateCounter() {
     let response = await fetch("https://apeppdbcgnkd5x7zcznqmpwy4a0dwpbc.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
-}
+} 
+
 updateCounter();
